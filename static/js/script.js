31ee41a021b9b0bd097cfa388859f6f84b0cb445
Sergej Felde <sfelde@gmail.com>
2012-05-14 01:17:22 +0200

    add hotline and search

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,18 @@
 /* Author:
-            MediaSapiens.co
-*/
-jQuery(function($) {
-    $('.login').click(function(e){
-       if(!$(this).hasClass('active'))
-       {
-           $(this).addClass('active');
-           $('.login__section').slideDown('fast');
-       }
-        else {
-           $(this).removeClass('active');
-           $('.login__section').slideUp('fast');
-       }
+ MediaSapiens.co
+ */
+jQuery(function ($) {
+    $('.showSection').click(function (e) {
+        $('header > nav > a.active').not(this).removeClass('active');
+        var _section = '.' + $(this).attr('data-layer');
+        if (!$(this).hasClass('active')) {
+            $('.hide__section').hide();
+            $(this).addClass('active');
+            $(_section).slideDown('fast');
+        } else {
+            $(this).removeClass('active');
+            $(_section).slideUp('fast');
+        }
     });
 });
 
